4063a561c9530a0b82878c622c0f02d8f882d414
Whiteknight <wknight8111@gmail.com>
2009-12-07 10:59:51 -0500

    start reworking indexing to be more sane, row-major

--- toolbox/is.m
+++ toolbox/is.m
@@ -3,7 +3,7 @@ function is(a, b, comment)
 %% tests that the two values a and b are equal. Calls ok() to handle
 %% the test output logic, so has all the same capabilities as that
 %% function.
-    if nargin == 2
+    if nargin == 3
         ok(a == b, comment);
     else
         ok(a == b);
